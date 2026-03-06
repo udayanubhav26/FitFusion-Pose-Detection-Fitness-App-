1637d0a0c5a5d4a0a12c4858ea83b15ec9de500a
Anubhav Sharma <anubhavsharma26114@gmail.com>
2026-03-06 12:15:40 +0530

    Initial commit

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'const.dart';
+// import 'const.dart';
 
 class ChatPage extends StatefulWidget {
   const ChatPage({super.key});
